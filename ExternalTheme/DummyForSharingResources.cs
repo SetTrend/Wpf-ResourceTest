755ecd55200af1864174c4aa9174afb4a6d18f25
Axel D <SetTrend@users.noreply.github.com>
2021-02-07 14:26:02 +0100

    Solution structure changed to contain two libraries:
    - Main app "ResourceTestApp"
    - Class library containing resources "ExternalResources"
    - Class library containing themes "ExternalThemes"
    
    In the current version, themes are not getting used - why?

--- ExternalTheme/DummyForSharingResources.cs
+++ ExternalTheme/DummyForSharingResources.cs
@@ -1,6 +1,4 @@
-﻿using System.Windows;
-
-namespace StylesAndResources
+﻿namespace ExternalTheme
 {
 	/* Um Ressourcen über Assemblies hinweg teilen zu können, ist eine Klasse notwendig,
 	 * mit dessen Hilfe der Pfad zur Ressouce von WPF erstellt werden kann.
@@ -8,7 +6,7 @@ namespace StylesAndResources
 	 * Es geht hier nur um ein Mittel zur automatischen Erzeugung einer eindeutigen Referenz-ID.
 	 * Die Klasse muss "public" definiert sein, damit sie von der anderen Assembly gefunden werden kann.
 	 */
-	public static class DummyForSharingResources
+	public abstract class DummyForSharingResources
 	{
 		//static public ComponentResourceKey TestImg = new ComponentResourceKey(typeof(DummyForSharingResources), "TestImg");
 		//static public ComponentResourceKey TestBg = new ComponentResourceKey(typeof(DummyForSharingResources), "TestBg");
---
+++ ExternalTheme/distinguishedGrids/Grid1.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ExternalTheme.distinguishedGrids
+{
+	/// <summary>
+	///		This type of <see cref="Grid"/> is defined here
+	///		merely to create a distinguished target type
+	///		to be used by styles.
+	/// </summary>
+	/// <remarks>
+	///		I could have used one of the WPF provided controls,
+	///		like <see cref="TextBlock"/>, <see cref="Button"/>,
+	///		<see cref="Border"/> etc., but I didn't want to deal
+	///		with their particular peculiarities. So I created
+	///		this <see cref="Grid"/> derived type for simplicity.
+	/// </remarks>
+	public class Grid1 : Grid
+	{
+		static Grid1()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(Grid1), new FrameworkPropertyMetadata());
+		}
+	}
+}
